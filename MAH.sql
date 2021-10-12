85caf6dc846d1f743054d2969db6daa934c69ce4
leorusso2300 <leorusso2607@gmail.com>
2021-10-12 09:02:52 -0300

    Scripts Banco

---
+++ MAH.sql
@@ -0,0 +1,3 @@
+select * from db_mah.tb_usuario;
+select * from db_mah.tb_postagem;
+select * from db_mah.tb_tema;
---
+++ blog_pessoal.sql
@@ -0,0 +1,7 @@
+SELECT * FROM blog_pessoal.tb_tema;
+SELECT * FROM blog_pessoal.tb_postagem;
+SELECT * FROM blog_pessoal.tb_usuario;
+
+
+insert into blog_pessoal.tb_usuario (nome,usuario,senha,data_nascimento) 
+values ("leo","leo","leo","1999/03/26");
